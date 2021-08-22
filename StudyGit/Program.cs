92332ac0bfccb05388138e04f0cc279c283f66df
Maksym <linnik.maksym@gmail.com>
2021-08-22 15:24:09 +0300

    first commit

---
+++ StudyGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudyGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
